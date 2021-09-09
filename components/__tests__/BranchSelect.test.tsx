2b4184fe99df2e993f07d5d1e4ec8b9a3f4cff54
Matt Rigdon <mrigdon@salesforce.com>
2021-09-09 07:54:04 -0700

    full test with msw

--- components/__tests__/BranchSelect.test.tsx
+++ components/__tests__/BranchSelect.test.tsx
@@ -1,11 +1,43 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { setupServer } from "msw/node";
 import BranchSelect from "../BranchSelect";
+import { rest } from "msw";
 
 describe("BranchSelect", () => {
-  it("renders a select", () => {
-    render(<BranchSelect value="main" onSelect={jest.fn()} />);
-    const select = screen.getByDisplayValue("main");
-    expect(select).toBeInTheDocument();
+  const server = setupServer(
+    rest.get(
+      "https://api.github.com/repos/netflix/eureka/branches",
+      (_, res, ctx) => res(ctx.json([{ name: "main" }, { name: "develop" }]))
+    )
+  );
+
+  beforeAll(() => server.listen());
+  afterEach(() => server.resetHandlers());
+  afterAll(() => server.close());
+
+  it("renders a select", async () => {
+    render(
+      <BranchSelect
+        org="netflix"
+        repo="eureka"
+        value="main"
+        onSelect={jest.fn()}
+      />
+    );
+
+    // initially main is selected
+    expect(screen.getByDisplayValue("main")).toBeInTheDocument();
+
+    // wait for the API call to return more
+    await waitFor(() => screen.getByText("develop"));
+
+    // select develop
+    fireEvent.select(screen.getByDisplayValue("main"), {
+      target: { value: "develop" },
+    });
+
+    // now develop is selected
+    expect(screen.getByDisplayValue("develop")).toBeInTheDocument();
   });
 });
--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,6 @@
 import "@testing-library/jest-dom/extend-expect";
+import fetch from "node-fetch";
+
+if (!globalThis.fetch) {
+  globalThis.fetch = fetch;
+}
